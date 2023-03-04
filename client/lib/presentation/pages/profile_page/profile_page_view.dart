109a743d3a86cf5c5b836832fb9fde8d76360352
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-04 23:36:33 +0100

    settings page basic ui

--- client/lib/presentation/pages/profile_page/profile_page_view.dart
+++ client/lib/presentation/pages/profile_page/profile_page_view.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
+import '../../../utils/extensions.dart';
+import '../../core/constants/dimensions.dart';
+import 'widgets/ex_widgets.dart';
+import 'widgets/greeting.dart';
+import 'widgets/settings_element/settings_element_log_in.dart';
+
 class ProfilePageView extends StatelessWidget {
   const ProfilePageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: horizontalPadding,
+          vertical: verticalPadding,
+        ),
+        child: Column(
+          children: [
+            const Greeting(),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  SettingsElementLogIn(
+                    title: context.l10n.settingsLogIn,
+                  ),
+                  SettingsElement(
+                    title: context.l10n.settingsCommunityGuidelines,
+                    onTap: () => print('redirect'),
+                  ),
+                  SettingsElement(
+                    title: context.l10n.settingsPrivacyPolicy,
+                    onTap: () => print('redirect'),
+                  ),
+                  SettingsElement(
+                    title: context.l10n.settingsTermsOfService,
+                    onTap: () => print('redirect'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ client/lib/presentation/pages/profile_page/widgets/settings_element/settings_element.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/extensions.dart';
+import '../../../../core/constants/colors.dart';
+import 'settings_element_core.dart';
+
+class SettingsElement extends StatelessWidget {
+  const SettingsElement({
+    Key? key,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  final String title;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return SettingsElementCore(title: title, onTap: onTap);
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(StringProperty('title', title))
+      ..add(ObjectFlagProperty<Function()>.has('onTap', onTap));
+  }
+}
