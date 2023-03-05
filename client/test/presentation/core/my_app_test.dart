5ee72bf1b31391d405edf4badbad5c7600e34c00
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-05 14:29:17 +0100

    resolve dart analyze

--- client/test/presentation/core/my_app_test.dart
+++ client/test/presentation/core/my_app_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:road_runner/presentation/core/my_app_widget.dart';
-import 'package:road_runner/presentation/pages/ex_pages.dart';
-import 'package:road_runner/presentation/router/router.dart';
 
 void main() {
   group('MyApp', () {
