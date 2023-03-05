03f4a4645141f1a8fd01fa041b84998790084add
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-05 14:25:31 +0100

    add test for MyApp widget

--- client/test/presentation/core/my_app_test.dart
+++ client/test/presentation/core/my_app_test.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:road_runner/presentation/core/my_app_widget.dart';
+import 'package:road_runner/presentation/pages/ex_pages.dart';
+import 'package:road_runner/presentation/router/router.dart';
 
 void main() {
   group('MyApp', () {
-    testWidgets('should render', (tester) async {
-      expect(1, 1);
+    testWidgets('renders SplashPage', (tester) async {
+      await tester.pumpWidget(
+        MyAppWidget(),
+      );
+      expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
 }
