77524f629da3fd644b4543bb3ed286d2079542cb
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-06 23:44:20 +0100

    server setup

--- server/users/cmd/app/main.go
+++ server/users/cmd/app/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	pb "github.com/wzslr321/road_runner/server/users/gen/proto"
+	"github.com/wzslr321/road_runner/server/users/settings"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+)
+
+var serverSettings *settings.Server
+
+type server struct {
+	pb.UnimplementedAuthServer
+}
+
+var devModeEnv = os.Getenv("DEV_MODE") // if not present, dev is used
+
+func init() {
+	devMode := settings.Dev
+	if devModeEnv == "PROD" {
+		devMode = settings.Prod
+	}
+
+	serverSettings = settings.GetServerConf(devMode)
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	listen, err := net.Listen("tcp", fmt.Sprintf("%s", serverSettings.Address))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterAuthServer(s, &server{})
+	log.Printf("Server listening on port %v", listen.Addr())
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
