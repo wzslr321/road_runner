977ababcd4fbfc26c9470a2889a354ad863b0002
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-13 16:15:14 +0100

    users service skeleton

---
+++ server/users/src/cmd/app/logging.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/wzslr321/road_runner/server/users/src/proto-gen"
+	"go.uber.org/zap"
+	"time"
+)
+
+type LoggingService struct {
+	logger *zap.Logger
+	child  Service
+}
+
+func NewLoggingService(child Service) Service {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	return &LoggingService{child: child, logger: logger}
+}
+
+func (s *LoggingService) GetUser(ctx context.Context) (user *pb.UserData, err error) {
+	defer func(start time.Time) {
+		s.logger.Info(fmt.Sprintf("user=%v\nerr=%s\ntook=%v", user, err, time.Since(start)))
+
+	}(time.Now())
+	return s.child.GetUser(ctx)
+}
--- server/users/src/go.mod
+++ server/users/src/go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
 	github.com/klauspost/cpuid/v2 v2.0.3 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
