60cfbdb4c5b5598c3f9b72dc2750916a5de80cd7
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-16 22:00:16 +0100

    boilerplate for fullfilling the service interface

--- server/users/src/api/api.go
+++ server/users/src/api/api.go
@@ -5,6 +5,26 @@ import (
 	pb "github.com/wzslr321/road_runner/server/users/src/proto-gen"
 )
 
-func (s *Server) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.UserData, error) {
-	return s.service.handleGetUser(ctx)
+func (s *Server) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	return s.service.handleGetUser(ctx, req)
+}
+
+func (s *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	return s.service.handleUpdateUser(ctx, req)
+}
+
+func (s *Server) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error) {
+	return s.service.handleDeleteUser(ctx, req)
+}
+
+func (s *Server) RegisterUser(ctx context.Context, req *pb.RegisterUserRequest) (*pb.RegisterUserResponse, error) {
+	return s.service.handleRegisterUser(ctx, req)
+}
+
+func (s *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	return s.service.handleLoginUser(ctx, req)
+}
+
+func (s *Server) LogoutUser(ctx context.Context, req *pb.LogoutUserRequest) (*pb.LogoutUserResponse, error) {
+	return s.service.handleLogoutUser(ctx, req)
 }
