1c0c7f3c4a4b46bac4a86c0182c95662b51d8b2e
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-26 03:59:44 +0200

    try to generate files on CI

--- proto/generate_users_server.sh
+++ proto/generate_users_server.sh
@@ -2,10 +2,6 @@
 
 readonly proto_file=$1
 readonly go_gen_path=$2
-readonly dart_gen_path=$3
-
-readonly curr_dir=$(pwd)
-readonly proto_file_path="${curr_dir}/${proto_file}.proto"
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
@@ -17,8 +13,4 @@ protoc --go_out="$go_gen_path" --go_opt=paths=source_relative \
     --go-grpc_out="$go_gen_path" --go-grpc_opt=paths=source_relative \
     "${proto_file}".proto
 
-dart pub global activate protoc_plugin
 
-export PATH="$PATH:$HOME/.pub-cache/bin"
-
-protoc --proto_path="$(pwd)" --dart_out=grpc:"$dart_gen_path" "$proto_file_path"
