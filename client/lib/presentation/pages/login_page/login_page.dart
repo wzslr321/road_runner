dc35393399fddd917cc34dacebc650da2c869eda
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-26 22:36:18 +0200

    login page files

---
+++ client/lib/presentation/pages/login_page/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'login_page_view.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginPageView();
+  }
+}
