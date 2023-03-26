ca2e23f6931033a9b3a34b9e6dfe044252b394f0
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-03-26 03:59:44 +0200

    try to generate files on CI

---
+++ proto/generate_users_client.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+readonly proto_file=$1
+readonly dart_gen_path=$2
+
+readonly curr_dir=$(pwd)
+readonly proto_file_path="${curr_dir}/${proto_file}.proto"
+
+dart pub global activate protoc_plugin
+
+export PATH="$PATH:$HOME/.pub-cache/bin"
+
+protoc --proto_path="$(pwd)" --dart_out=grpc:"$dart_gen_path" "$proto_file_path"
